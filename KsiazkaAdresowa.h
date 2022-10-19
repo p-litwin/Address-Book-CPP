4cb85d95133d30b23d3849780cf8fb5c9c85bdb5
plitwin <plitwin@borgwarner.com>
2022-10-19 15:35:28 +0200

    Created classes KsiazkaAdresowa and Uzytkownik

---
+++ KsiazkaAdresowa.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class KsiazkaAdresowa {
+    int idZalogowanegoUzytkownika;
+    int idOstatniegoAdresata;
+    int idUsunietegoAdresata;
+
+    vector <Uzytkownik> uzytkownicy;
+
+public:
+    void rejestracjaUzytkownika() {
+
+    }
+
+};
---
+++ Uzytkownik.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+class Uzytkownik {
+    int id = 0;
+    string login = "", haslo = "";
+};
